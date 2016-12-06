f2edd89b48f76f264e8f0df46df7df41a3db468b
Bahaa Aidi <bahaa.aidi@gmail.com>
2016-12-06 23:45:31 +0200

    add 'zip' task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = function(grunt) {
     var pkg = grunt.file.readJSON('package.json');
     grunt.initConfig({
@@ -55,7 +57,18 @@ module.exports = function(grunt) {
             dist : {
                 files: {'dist/hola_player.min.js' : 'dist/hola_player.js'},
             },
-        }
+        },
+        zip: {
+            dist: {
+                router: function(filepath){
+                    if (path.extname(filepath)=='.zip')
+                        return null;
+                    return path.relative('dist', filepath);
+                },
+                src: ['dist/**/*'],
+                dest: 'dist/hola-player-'+ pkg.version+'.zip'
+            },
+        },
     });
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-browserify');
@@ -63,6 +76,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-exorcise');
+    grunt.loadNpmTasks('grunt-zip');
     grunt.registerTask('build',
         ['clean', 'jshint', 'browserify:dist', 'exorcise', 'copy', 'uglify']);
     grunt.registerTask('default', ['build']);
