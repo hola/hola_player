409479a9bfa034cca03d15dc68678ffd3aabb94c
Bahaa Aidi <bahaa.aidi@gmail.com>
2016-12-21 19:29:50 +0200

    use @hola.org/videojs5-hlsjs-source-handler

--- src/hlsjs_source_handler.js
+++ src/hlsjs_source_handler.js
@@ -1,7 +1,7 @@
 'use strict';
 var videojs = require('video.js');
 var Hls = require('hls.js');
-var hlsjs_source_handler = require('videojs5-hlsjs-source-handler');
+var hlsjs_source_handler = require('@hola.org/videojs5-hlsjs-source-handler');
 
 module.exports = function(){
     hlsjs_source_handler.attach(window, videojs, Hls);
