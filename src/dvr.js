ccaee54ffe6dc29d05deac984dc5c5d0ceff68f8
Andrey Kashta <andrey@hola.org>
2017-02-27 22:36:11 +0300

    implement dvr seek bar

--- src/dvr.js
+++ src/dvr.js
@@ -10,7 +10,7 @@ vjs.plugin('dvr', function(){
         player.controlBar.addClass('vjs-dvr');
         var progressControl = player.controlBar.progressControl;
         progressControl.removeChild('seekBar');
-        progressControl.addChild('DvrSeekBar');
+        progressControl.seekBar = progressControl.addChild('DvrSeekBar');
     });
     player.dvr = {
         range: function(){
@@ -23,17 +23,27 @@ vjs.plugin('dvr', function(){
             var end = range && range.end;
             return end && player.currentTime() >= end;
         },
+        format_time: function(time){
+            var range = player.dvr.range();
+            if (!range)
+                return '0:00';
+            if (!time)
+            {
+                time = player.scrubbing() ? player.getCache().currentTime :
+                    player.currentTime();
+            }
+            time = Math.max(range.end-time, 0);
+            return (time > 0 ? '-' : '')+vjs.formatTime(time, range.end);
+        },
     };
 });
 
 var SeekBar = vjs.getComponent('SeekBar');
 vjs.registerComponent('DvrSeekBar', vjs.extend(SeekBar, {
     options_: {
-        children: ['loadProgressBar', 'mouseTimeDisplay', 'playProgressBar'],
-        barName: 'playProgressBar'
-    },
-    constructor: function(player, options){
-        SeekBar.call(this, player, options);
+        children: ['dvrLoadProgressBar', 'dvrMouseTimeDisplay',
+            'dvrPlayProgressBar'],
+        barName: 'dvrPlayProgressBar'
     },
     // XXX andrey: implement updateAriaAttributes, stepForward, stepBack
     getPercent: function(){
@@ -53,7 +63,93 @@ vjs.registerComponent('DvrSeekBar', vjs.extend(SeekBar, {
     },
 }));
 
-// XXX andrey: implement custom PlayProgressBar
-// XXX andrey: implement custom LoadProgressBar
-// XXX andrey: implement custom MouseTimeDisplay
+var PlayProgressBar = vjs.getComponent('PlayProgressBar');
+vjs.registerComponent('DvrPlayProgressBar', vjs.extend(PlayProgressBar, {
+    updateDataAttr: function(){
+        this.el_.setAttribute('data-current-time',
+            this.player_.dvr.format_time());
+    },
+}));
+
+function el_pos(el){
+    var box;
+    if (el.getBoundingClientRect && el.parentNode)
+        box = el.getBoundingClientRect();
+    else
+        return 0;
+    var body = document.body;
+    var client = document.documentElement.clientLeft || body.clientLeft || 0;
+    var scroll = window.pageXOffset || body.scrollLeft;
+    return Math.round(box.left + scroll - client);
+}
+
+var MouseTimeDisplay = vjs.getComponent('MouseTimeDisplay');
+vjs.registerComponent('DvrMouseTimeDisplay', vjs.extend(MouseTimeDisplay, {
+    handleMouseMove: function(event){
+        var range = this.player_.dvr.range();
+        var time = range ? this.calculateDistance(event) *
+            (range.end-range.start) + range.start : 0;
+        var max_left = this.player_.controlBar.progressControl.seekBar.width()-
+            this.width();
+        var pos = event.pageX - el_pos(this.el().parentNode);
+        pos = Math.min(Math.max(0, pos), max_left);
+        var tp_width = this.tooltip.offsetWidth;
+        var max_tp_left = this.tooltip.parentNode.offsetWidth-tp_width;
+        var tp_pos = event.pageX-el_pos(this.tooltip.parentNode)-tp_width/2;
+        tp_pos = Math.min(Math.max(0, tp_pos), max_tp_left);
+        this.update(time, pos, tp_pos);
+    },
+    update: function(time, pos, tp_pos){
+        this.el().style.left = pos + 'px';
+        this.tooltip.innerHTML = this.player_.dvr.format_time(time);
+        if (this.keepTooltipsInside)
+        {
+            var diff = pos-this.clampPosition_(pos)+1;
+            var width =
+                parseFloat(window.getComputedStyle(this.tooltip).width);
+            this.tooltip.style.left = 'auto';
+            this.tooltip.style.right = '-' +(width/2-diff)+'px';
+        }
+        else
+        {
+            this.tooltip.style.right = 'auto';
+            this.tooltip.style.left = tp_pos + 'px';
+        }
+    },
+}));
+
+var LoadProgressBar = vjs.getComponent('LoadProgressBar');
+vjs.registerComponent('DvrLoadProgressBar', vjs.extend(LoadProgressBar, {
+    constructor: function(player, options){
+        LoadProgressBar.call(this, player, options);
+        this.partEls_ = [];
+        if (player.tech_.hls_obj)
+            this.on(player.tech_.hls_obj, 'hlsLevelUpdated', this.update);
+    },
+    update: function(){
+        var percentify = function(time, end){
+            var percent = time / end || 0;
+            return (percent >= 1 ? 1 : percent) * 100 + '%';
+        };
+        var buff = this.player_.buffered();
+        var range = this.player_.dvr.range();
+        var start = range ? range.start : 0;
+        var end = range ? range.end : 0;
+        var buf_end = Math.min(this.player_.bufferedEnd(), end) - start;
+        var children = this.partEls_;
+        var i;
+        this.el_.style.width = percentify(buf_end, end-start);
+        for (i = 0; i < buff.length; i++)
+        {
+            children[i] = children[i] || this.el_.appendChild(vjs.createEl());
+            var part = children[i];
+            part.style.left = percentify(buff.start(i)-start, buf_end);
+            part.style.width = percentify(buff.end(i)-buff.start(i), buf_end);
+        }
+        for (i = children.length; i > buff.length; i--)
+            this.el_.removeChild(children[i-1]);
+        children.length = buff.length;
+    },
+}));
+
 // XXX andrey: implement LiveButton
